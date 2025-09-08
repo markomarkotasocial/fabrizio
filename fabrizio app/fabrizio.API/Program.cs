c5b84d9620bbff6a950096185ad9cfbd8e07a841
Marko Markota <markota@gmail.com>
2025-09-08 08:06:54 +0200

    Program.cs fixes

--- fabrizio app/fabrizio.API/Program.cs
+++ fabrizio app/fabrizio.API/Program.cs
@@ -71,18 +71,14 @@ builder.Services.AddSwaggerGen(c =>
 
 
 builder.Services.AddAuthorization();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();  
